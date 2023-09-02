051b82e6d5a7039dab3a256b7205adb265e1e053
ishara111 <isharagomes111@gmail.com>
2023-09-02 10:49:14 +0530

    score hit 50 added

--- src/main/java/edu/curtin/sec/assignment1/Score.java
+++ src/main/java/edu/curtin/sec/assignment1/Score.java
@@ -1,60 +1,59 @@
-//package edu.curtin.sec.assignment1;
-//
-//import javafx.application.Platform;
-//import javafx.scene.control.Label;
-//
-//public class Score implements Runnable{
-//
-//    //  private Object monitor = new Object();
-//    private int score;
-//    private App app;
-//    //  private Label scoreText;
-//
-//    public Score(App app) {
-//        this.score = 0;
-//        // this.scoreText =scoreText;
-//        this.app = app;
-//    }
-//
-//    public int getScore() {
-//
-//        return score;
-//    }
-//
-//    private void incrementScore() {
-////        synchronized(monitor)
-////        {
-//        this.score++ ;
-//
-//    public void getScoreText() {
-//        Platform.runLater(() -> {
-//
-//            this.app.changeScore(this.getScore());
-//        });
-//
-//    }
-//
-//    public void setScoretext1(){
-//            Platform.runLater(() -> {
-//
-//                this.app.changeScore(this.getScore());
-//            });
-//        }
-//
-//    @Override
-//    public void run() {
-//        while (true) {
-//            // Your code here
-//            incrementScore();
-//            setScoreText();
-//            System.out.println(getScore());
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                System.out.println("Goodbye !!!!");
-//                break;
-//            }
-//        }
-//    }
-//}
-//
+package edu.curtin.sec.assignment1;
+
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+
+public class Score implements Runnable{
+
+    //  private Object monitor = new Object();
+    private int score;
+    private App app;
+    //  private Label scoreText;
+
+    public Score(App app) {
+        this.score = 0;
+        // this.scoreText =scoreText;
+        this.app = app;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    private void incrementScore() {
+//        synchronized(monitor)
+//        {
+        this.score++ ;
+//            monitor.notifyAll();
+////        }
+
+    }
+
+    public void setScoreText() {
+        Platform.runLater(() -> {
+
+            this.app.changeScore(this.getScore());
+        });
+
+    }
+
+    public void addScoreFifty()
+    {
+        this.score = this.score+50;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            incrementScore();
+            setScoreText();
+            System.out.println(getScore());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Goodbye !!!!");
+                break;
+            }
+        }
+    }
+}
